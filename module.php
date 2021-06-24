9ef439942a9380e553125d63d64cb83b882178d8
Evan Donnel <evandonnel@gmail.com>
2021-06-24 07:53:23 -0400

    Changed config
    Directory constants are empty by default. Module will die() if directory constants are not set.

--- module.php
+++ module.php
@@ -2,6 +2,10 @@
 	ini_set('display_errors', 1);
 
 	require_once __DIR__.'/_config.php';
+
+	if (!DIR_PROD || !DIR_STAG)
+		die('Please set directories to compare.');
+
 	require_once __DIR__.'/functions.php';
 	require_once __DIR__.'/_globals.php';
 
