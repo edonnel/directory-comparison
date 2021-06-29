4f74eaa165d119037d0cc11212ea6dcb732e6153
Evan Donnel <e.donnel@epicwebstudios.com>
2021-06-29 19:54:48 -0400

    Cleanup and preparation for extensibility

--- globals.php
+++ globals.php
@@ -1,15 +1,16 @@
 <?
 	global $db;
-
 	const THIS_DIR = __DIR__;
+	define('DOCUMENT_ROOT', $_SERVER['DOCUMENT_ROOT']);
+	define('REAL_ROOT', realpath($_SERVER['DOCUMENT_ROOT']));
 	define('THIS_URL', '?a='.$_GET['a']);
-	define('ACCOUNT_ROOT', dirname($_SERVER['DOCUMENT_ROOT']));
+	define('ACCOUNT_ROOT', dirname(DOCUMENT_ROOT));
 
 	const PATH_PROD = ACCOUNT_ROOT.'/'.DIR_PROD;
 	const PATH_STAG = ACCOUNT_ROOT.'/'.DIR_STAG;
 
 	if (!isset($db))
-		require_once BASE_DIR.'/sources/init/db.config.php';
+		require_once DOCUMENT_ROOT.'/sources/init/db.config.php';
 
 	$conn = new mysqli($db['host'], $db['user'], $db['pass'], $db['db']);
 
@@ -19,4 +20,4 @@
 	define('URL', $conn->query("SELECT `url` FROM `settings` LIMIT 1")->fetch_object()->url);
 
 	const THIS_URL_FULL = URL.'/admin/'.THIS_URL;
-	define('THIS_URL_DIR', URL.(str_replace(BASE_DIR, '', THIS_DIR)));
+	define('THIS_URL_DIR', URL.(str_replace(REAL_ROOT, '', THIS_DIR)));
